1c9d227562e611522b0314e552b4545580581be4
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2024-04-30 14:44:26 -0300

    feat: ExerciseList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,23 @@
+import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { FlatList, HStack, VStack } from '@gluestack-ui/themed';
+import { FlatList, HStack, Heading, Text, VStack } from '@gluestack-ui/themed';
 import { useState } from 'react';
 
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState('Costas');
   const [groups, setGroups] = useState([
     'Costas',
     'Bíceps',
     'Tríceps',
     'ombro',
   ]);
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terras',
+  ]);
+  const [groupSelected, setGroupSelected] = useState('Costas');
 
   return (
     <VStack flex={1}>
@@ -33,6 +40,26 @@ export function Home() {
         my={'$10'}
         maxHeight={'$10'}
       />
+
+      <VStack px={'$8'}>
+        <HStack justifyContent="space-between" mb={'$5'} alignItems="center">
+          <Heading color="$coolGray200" fontSize="$md" lineHeight={'$2xl'}>
+            Exercícios
+          </Heading>
+
+          <Text color="$coolGray200" fontSize="$sm" lineHeight={'$2xl'}>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item): any => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 32 }}
+        />
+      </VStack>
     </VStack>
   );
 }
