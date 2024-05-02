7c277c0ca04d3d68dd767b992ca29bfa3514e0e6
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2024-05-02 17:04:39 -0300

    feat: image picker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,15 +1,83 @@
-import { Center, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import {
+  Center,
+  ScrollView,
+  Text,
+  Toast,
+  ToastTitle,
+  VStack,
+  useToast,
+  useStyled,
+} from '@gluestack-ui/themed';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 
 import { Input } from '@components/Input';
-
+import * as ImagePicker from 'expo-image-picker';
 import { TouchableOpacity } from 'react-native';
 import { Heading } from '@gluestack-ui/themed';
 import { Button } from '@components/Button';
+import { useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as FileSystem from 'expo-file-system';
 
 export function Profile() {
+  const toast = useToast();
+  const { space } = useStyled().config.tokens;
+
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/FPPaschoal.png'
+  );
+  async function handleUserPhotoSelected() {
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      aspect: [4, 4],
+      allowsEditing: true,
+    });
+
+    if (photoSelected.canceled) {
+      return;
+    }
+    if (photoSelected.assets[0].uri) {
+      const photoInfo = await FileSystem.getInfoAsync(
+        photoSelected.assets[0].uri,
+        { size: true }
+      );
+
+      if (photoInfo.exists && photoInfo.size / 1024 > 2) {
+        return toast.show({
+          placement: 'top',
+          render: ({ id }) => {
+            const toastId = 'toast-' + id;
+            return (
+              <Toast
+                bg="$error600"
+                nativeID={toastId}
+                p="$4"
+                gap={'$4'}
+                maxWidth={'$4/5'}
+                alignSelf="center"
+                justifyContent="center"
+                alignItems="center"
+                mt={'$10'}
+              >
+                <MaterialIcons name="warning" color={'white'} size={space[6]} />
+                <VStack space="xs">
+                  <ToastTitle color="$textLight50">
+                    Essa imagem é muito grande. Escolha uma de até 5MB
+                  </ToastTitle>
+                </VStack>
+              </Toast>
+            );
+          },
+        });
+      }
+
+      setUserPhoto(photoSelected.assets[0].uri);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -19,10 +87,10 @@ export function Profile() {
           <UserPhoto
             w={'$32'}
             h={'$32'}
-            source={{ uri: 'https://github.com/FPPaschoal.png' }}
+            source={{ uri: userPhoto }}
             alt="Imagem do usuário"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
               color="$green500"
               fontWeight="bold"
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -13,12 +13,35 @@ import LogoSvg from '@assets/logo.svg';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
 
 export function SignUp() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
   }
 
   return (
@@ -54,32 +77,86 @@ export function SignUp() {
               >
                 Crie sua Conta
               </Heading>
-              <Input
-                InputFieldProps={{
-                  placeholder: 'Nome',
+              <Controller
+                control={control}
+                name="name"
+                rules={{
+                  required: 'Informe o nome.',
                 }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    InputFieldProps={{
+                      placeholder: 'Nome',
+                      onChangeText: onChange,
+                      value: value,
+                    }}
+                  />
+                )}
               />
-              <Input
-                InputFieldProps={{
-                  placeholder: 'E-mail',
-                  autoCapitalize: 'none',
+              <Text color="white">{errors.name?.message}</Text>
+              <Controller
+                control={control}
+                name="email"
+                rules={{
+                  required: 'Informe o email.',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'E-mail inválido',
+                  },
                 }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    InputFieldProps={{
+                      placeholder: 'E-mail',
+                      autoCapitalize: 'none',
+                      onChangeText: onChange,
+                      value: value,
+                      keyboardType: 'email-address',
+                    }}
+                  />
+                )}
               />
-              <Input
-                InputFieldProps={{
-                  placeholder: 'Senha',
-                  type: 'password',
-                  secureTextEntry: true,
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    InputFieldProps={{
+                      placeholder: 'Senha',
+                      type: 'password',
+                      secureTextEntry: true,
+                      onChangeText: onChange,
+                      value: value,
+                    }}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="password_confirm"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    InputFieldProps={{
+                      placeholder: 'Confirme a senha',
+                      type: 'password',
+                      secureTextEntry: true,
+                      onChangeText: onChange,
+                      value: value,
+                      returnKeyType: 'send',
+
+                      onSubmitEditing: () => handleSubmit(handleSignUp),
+                    }}
+                  />
+                )}
+              />
+
+              <Button
+                GluestackButtonProps={{
+                  onPress: handleSubmit(handleSignUp),
                 }}
-              />
-              <Input
-                InputFieldProps={{
-                  placeholder: 'Confirme a senha',
-                  type: 'password',
-                  secureTextEntry: true,
-                }}
-              />
-              <Button>Criar e acessar</Button>
+              >
+                Criar e acessar
+              </Button>
             </Center>
           </View>
 
